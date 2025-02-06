ecec75ff9cd50f7baa3a8b326b32377d6c57450d
Bisal Ghora <111360451+Bisal4u@users.noreply.github.com>
2025-02-06 10:37:58 +0530

    Update and rename Programming Assignment 1 to Programming Assignment 1.py

---
+++ Week-3/Programming Assignment 1.py
@@ -0,0 +1,10 @@
+
+numbers = list(map(int,input().split()))
+
+unique_numbers=sorted(set(numbers))
+length=len(unique_numbers)
+
+if length<2:
+    print("No smallest number")
+else:
+    print(unique_numbers[1])
