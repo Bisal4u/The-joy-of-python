ba53d0333abe760a905b45ca78c85f8449655dd8
Bisal Ghora <111360451+Bisal4u@users.noreply.github.com>
2025-02-09 10:33:13 +0530

    Update Programming Assignment 2.py

--- Week-3/Programming Assignment 2.py
+++ Week-3/Programming Assignment 2.py
@@ -1,1 +1,21 @@
+user_input=list(map(int,input().split()))
 
+def duplicates_remover(numbers):
+    seen_f1=set()
+    even_f2=set()
+    result=[]
+    
+    for i in numbers:
+        if i%2==0:
+            if i in even_f2:
+                continue
+            even_f2.add(i)
+            
+        result.append(i)
+        
+    return result
+            
+
+output=duplicates_remover(user_input)
+
+print(" ".join(map(str,output)),end="")
